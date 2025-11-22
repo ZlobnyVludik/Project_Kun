1a4c10f654923eef7c7c2accb7e62f080c15bf06
ZlobnyVludik <kunvlad24@gmail.com>
2025-11-22 16:03:10 +0300

    6 практос

---
+++ PZ_6/PZ_6_1.py
@@ -0,0 +1,35 @@
+# Дан целочисленный список размера 10. Вывести все содержащиеся в данном списе
+# четные числа в порядке убывания их индексов, а также их количество K.
+
+def chet_chisla():
+    # Создаем список
+    numbers = []
+    print("Введите 10 чисел: ")
+
+    # Получаем значения
+    for i in range(10):
+        while True:
+            try:
+                value = int(input(""))
+                numbers.append(value)
+                break
+            except ValueError:
+                print("Нужны целые числа.")
+
+    # Сохраняем чётные числа
+    cel_numbers = []
+    count_numbs = 0
+
+    # Перебираем индексы
+    for index in range(9, -1, -1):
+        if numbers[index] % 2 == 0:
+            cel_numbers.append(numbers[index])
+            count_numbs += 1
+
+    # Выводим
+    print("\nЧётные цифры в порядке идекса: ")
+    for num in cel_numbers:
+        print(num)
+    print(f"\nКол-во чётных чисел: {count_numbs}")
+
+chet_chisla()
---
+++ PZ_6/PZ_6_2.py
@@ -0,0 +1,40 @@
+# Дан список размера N. Найти кол-во участков, на которых его элементы монотонно возрастают.
+
+def incresing_numbers():
+    while True:
+        try:
+            # Получаем значения
+            n = int(input("Введите размер списка: "))
+            if n <= 0:
+                print("Только положительные.")
+                continue
+            break
+        except ValueError:
+            print("Целые числа.")
+
+    numbers = []
+    print(f"\nВведите {n} чисел: ")
+
+    for i in range(n):
+        while True:
+            try:
+                value = int(input(f"Элемент {i}: "))
+                numbers.append(value)
+                break
+            except ValueError:
+                print("Целые числа!!!")
+
+    count_segments = 0
+    in_segment = False
+
+    for i in range(1, n):
+        if numbers[i] > numbers[i - 1]:
+            if not in_segment:
+                count_segments += 1
+                in_segment = True
+        else:
+            in_segment = False
+
+    print(f"Кол-во участков: {count_segments}")
+
+incresing_numbers()
