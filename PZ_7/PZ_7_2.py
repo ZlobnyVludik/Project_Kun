c43ba87a5c1317cbe3e489503814707b3b579ba5
ZlobnyVludik <152613113+ZlobnyVludik@users.noreply.github.com>
2025-11-26 18:58:43 +0300

    Update PZ_7_2.py
    Работа

--- PZ_7/PZ_7_2.py
+++ PZ_7/PZ_7_2.py
@@ -1,1 +1,18 @@
 
+# Дана строка-предложение с избыточными пробелами между словами.
+# Преобразовать ее так, чтобы между словами был ровно один пробел.
+
+def probel(sentence):
+    try:
+        # Проверяем, чтобы кд был строкой
+        if not isinstance(sentence, str):
+            raise ValueError("Только строка.")
+        # Исправляем пробелы
+        return " ".join(sentence.split())
+    except Exception:
+        return "Ошибка!"
+        
+# Вывод
+input_sentence = input("Введите предложение с лишними пробелами: ")
+result = probel(input_sentence)
+print(result)
