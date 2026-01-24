f7a76660b4bb20c800f4f338e3d7374132aed208
ZlobnyVludik <152613113+ZlobnyVludik@users.noreply.github.com>
2026-01-24 10:32:15 +0300

    Update PZ_8_1.py

--- PZ_8/PZ_8_1.py
+++ PZ_8/PZ_8_1.py
@@ -1,1 +1,12 @@
+#Извлеките ключи ["name", "salary"] из sample_dict.
 
+sample_dict = {
+ "name": "Kelly",
+ "age":25,
+ "salary": 8000,
+ "city": "New york"
+}
+
+name = sample_dict["name"]
+salary = sample_dict["salary"]
+print(f"Имя: {name}\nЗарплата: {salary}")
