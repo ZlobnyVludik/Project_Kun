dc0f15b0f361301835cc9c282d091b2279625bf5
ZlobnyVludik <152613113+ZlobnyVludik@users.noreply.github.com>
2026-01-31 11:06:53 +0300

    Update PZ_9_1.py

--- PZ_9/PZ_9_1.py
+++ PZ_9/PZ_9_1.py
@@ -13,11 +13,20 @@ pyatka = {"Мясо", "Молоко", "Сыр"}
 prkrst = {"Молоко", "Творог", "Сыр", "Сахар"}
 
 # Список
-stores = {
-    "Магнит": magnit,
-    "Пятерочка": pytka,
-    "Перекресток": prkrst
+stores_dict = {
+    "Магните": magnit,
+    "Пятерочке": pyatka,
+    "Перекрестке": prkrst
 }
 
 # Задание 1
+no_cheese = [stores for stores, stock in stores_dict.items() if "Сыр" not in stock]
+print("Сыра нет в", no_cheese)
 
+# Задание 2
+m_and_s = [stores for stores, stock in stores_dict.items() if {"Молоко", "Сахар"}.issubset(stock)]
+print("Молоко и сахар можно купить в", m_and_s)
+
+# Задание 3
+salty = [stores for stores, stock in stores_dict.items() if "Соль" in stock]
+print("Соль можно купить в", salty)
