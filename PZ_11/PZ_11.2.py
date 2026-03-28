e8a8379c33d4f6c289b31d10bf70226a7c2afafe
ZlobnyVludik <152613113+ZlobnyVludik@users.noreply.github.com>
2026-03-28 10:50:58 +0300

    Update PZ_11.2.py

--- PZ_11/PZ_11.2.py
+++ PZ_11/PZ_11.2.py
@@ -1,13 +1,16 @@
 # Составить генератор (yield), который преобразует
 # все буквенные символы в заглавные.
 
+# Функция для преобрзования символа в верхний регистр
 def to_upper(text):
     for char in text:
         yield char.upper()
 
+# Получаем текст
 small_text = input("Введите текст: ")
 up = to_upper(small_text)
 
+# Помещаем и выводим
 big_text = "".join(up)
 print(big_text)
     
