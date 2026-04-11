696ac0d739abf86f01a86fe528e8983826dcd9f2
ZlobnyVludik <152613113+ZlobnyVludik@users.noreply.github.com>
2026-04-11 10:19:14 +0300

    Update PZ_12_2.py

--- PZ_12/PZ_12_2.py
+++ PZ_12/PZ_12_2.py
@@ -1,1 +1,15 @@
 
+# Ввод размеров матрицы
+n = int(input("Количество строк: "))
+m = int(input("Количество столбцов: "))
+
+# Ввод матрицы
+matrix = [list(map(int, input(f"Строка {i + 1}: ").split())) for i in range(n)]
+
+# Последнюю строку заменяем на 0
+matrix[-1] = [0 for _ in range(m)]
+
+# Вывод результата
+print("Изменённая матрица:")
+for row in matrix:
+    print(*row)
