a984437924e1b9ed13905a4c037639724b0c4b4d
ZlobnyVludik <152613113+ZlobnyVludik@users.noreply.github.com>
2026-04-25 09:34:46 +0300

    Update PZ_12_1.py
    complete

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -1,10 +1,19 @@
 # В матрице элементы столбца N (N задать с клавиатуры) увеличить в 2 раза.
 
-matrix = [list(map(int, input().split())) for _ in range(int(input()))]
-N = int(input()) - 1
+rows = int(input("Введите кол-во строк: "))
+
+matrix = []
+for i in range(rows):
+    row = list(map(int, input("Введите элементы строки (через пробел): ").split()))
+    matrix.append(row)
+
+N = int(input("Введите номер стобца: ")) - 1
 
 for row in matrix:
-    row[N] *= 2
+    if N < len(row):
+        row[N] *= 2
 
+print("Результат: \n")
 for row in matrix:
-    print(row)
+    print(*row)
+    
