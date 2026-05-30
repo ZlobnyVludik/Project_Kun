5acf7facb8810ca836d9e6004903fea013872ecc
ZlobnyVludik <152613113+ZlobnyVludik@users.noreply.github.com>
2026-05-30 12:21:47 +0300

    Update PZ_16_2.py

--- PZ_16/PZ_16_2.py
+++ PZ_16/PZ_16_2.py
@@ -3,4 +3,46 @@
 # и "питаться", а классы-наследники будут иметь свои уникальные методы и свойства,
 # такие как "гавкать" и "мурлыкать". 
 
+class Animal:
+    def __init__(self, name):
+        self.name = name
 
+    def breathe(self):
+        print(f"{self.name} дышит.")
+
+    def eat(self):
+        print(f"{self.name} питается.")
+
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
+
+    def bark(self):
+        print(f"{self.name} гавкает: Гав-гав!")
+
+
+class Cat(Animal):
+    def __init__(self, name, color):
+        super().__init__(name)
+        self.color = color
+
+    def purr(self):
+        print(f"{self.name} мурлычет: Мур-мур!")
+
+
+# Создание объектов
+dog = Dog("Бобик", "Овчарка")
+cat = Cat("Мурка", "Рыжий")
+
+# Методы базового класса
+dog.breathe()
+dog.eat()
+
+cat.breathe()
+cat.eat()
+
+# Уникальные методы
+dog.bark()
+cat.purr()
