4ab411d85e64b061d3201511fcac142cb09ab567
ZlobnyVludik <152613113+ZlobnyVludik@users.noreply.github.com>
2026-06-06 12:19:17 +0300

    Update PZ_16_1.py

--- PZ_16/PZ_16_1.py
+++ PZ_16/PZ_16_1.py
@@ -1,15 +1,1 @@
-# Создайте класс "Человек" с атрибутами "имя", "возраст" и "пол".
-# Напишите метод, который выводит информацию о человеке в формате
-# "Имя: имя, Возраст: возраст, Пол: пол".
 
-class Human:
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
-
-    def show_info(self):
-        print(f"Имя: {self.name}, Возраст: {self.age}, Пол: {self.gender}")
-
-person = Human("Иван", 25, "мужской")
-person.show_info()
