077dac5e0c20028f952eefdbb86cfda41f2b303f
ZlobnyVludik <152613113+ZlobnyVludik@users.noreply.github.com>
2026-06-13 11:15:22 +0300

    Update PZ_14_2.py

--- PZ_14/PZ_14_2.py
+++ PZ_14/PZ_14_2.py
@@ -1,35 +1,25 @@
 from tkinter import *
 from tkinter import ttk
 
-
 root = Tk()
-root.title("Конвертер размера файла")
+root.title("К ПЗ 2")
 root.geometry("300x150")
 
 
 def calculate():
-    try:
-        bytes_val = int(entry.get())
-        kilobytes = bytes_val // 1024
-        result_label.config(text=f"Килобайт: {kilobytes} КБ")
-    except ValueError:
-        result_label.config(text="Введите целое число!")
+    byt = int(entry.get())
+    kilobytes = byt // 1024
+    result_label.config(text=f"Килобайт: {kilobytes} КБ")
 
 
-def close():
-    root.destroy()
-    root.quit()
+Label(text="Введите размер файла (байт):").pack(pady=5)
 
-
-Label(root, text="Введите размер файла (байт):").pack(pady=5)
-
-entry = ttk.Entry(root, width=25)
-entry.pack(pady=5)
+entry = ttk.Entry(width=25)
+entry.pack()
 
 ttk.Button(root, text="Вычислить", command=calculate).pack(pady=5)
 
-result_label = Label(root, text="Килобайт: —")
+result_label = Label(text="Килобайт: —")
 result_label.pack(pady=5)
 
-root.protocol("WM_DELETE_WINDOW", close)
 root.mainloop()
