4fbb6c1ecce4d12033458ab90f12136cd0786f1e
ZlobnyVludik <152613113+ZlobnyVludik@users.noreply.github.com>
2026-06-16 20:39:59 +0300

    Update PZ_12_1.py

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -5,17 +5,17 @@ import random
 rows = int(input("Количество строк: "))
 columns = int(input("Количество столбцов: "))
 
-matrix = [[random.randint(1, 20) for a in range(columns)] for i in range(rows)]
+matrix = [[random.randint(1, 20) for j in range(columns)] for i in range(rows)]
 
 print("Исходная матрица: ")
-for row in matrix:
-    print(row)
+for column in matrix:
+    print(column)
 
 n = int(input("\nВведите номер столбца: "))
 
-for i in range(rows):
+for i in range(columns):
     matrix[i][n - 1] *= 2
 
 print("Измененная матрица: ")
-for row in matrix:
-    print(row)
+for column in matrix:
+    print(column)
